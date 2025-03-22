68c00b81f6b602606d043959ea2ac8a6b8cd0575
Amit Kumar <aktech@users.noreply.github.com>
2025-03-22 15:28:36 +0000

    add example for running an ubuntu vm

---
+++ start.sh
@@ -0,0 +1,9 @@
+./create-cloud-init.sh
+./cloud-hypervisor \
+        --kernel ./hypervisor-fw \
+        --disk path=focal-server-cloudimg-amd64.raw path=/tmp/ubuntu-cloudinit.img \
+        --cpus boot=4 \
+        --memory size=1024M \
+        --net "tap=,mac=,ip=,mask=" \
+        --serial tty \
+        --console off
